c0602ea8288da9b1394f639a15bbee7b392fb66e
king <960889@qq.com>
2019-05-14 17:28:12 +0800

    将文件变成类。

--- scripts/1_get_pics_from_web_page.py
+++ scripts/1_get_pics_from_web_page.py
@@ -1,5 +1,4 @@
 import os
-from threading import Thread
 import urllib.request
 import socket
 import json
@@ -8,166 +7,160 @@ import hashlib
 import requests
 from concurrent.futures import ThreadPoolExecutor
 import threading
+from threading import Thread
 
-class MonitorThread(Thread):
 
-    def __init__(self, name, args):
-        super().__init__()
-        self.name = name
-        self.args = args
-        print('Thread : ' + str(self.args) + ', ' + self.name + ' start')
+class GetPicsFromWebPage:
+    def printE(self, s):
+        print('\033[31m ' + s + ' \033[0m')
 
-    def run(self):
-        proxy_support = urllib.request.ProxyHandler({'sock5': 'localhost:1080'})
-        opener = urllib.request.build_opener(proxy_support)
-        urllib.request.install_opener(opener)
-        #data = urllib.request.urlopen(self.name).read()
-        json = urllib.request.urlopen(self.name).read().decode("utf8")
-        print(json)
+    def printI(self, s):
+        print('\033[32m ' + s + ' \033[0m')
 
-def printE(s):
-    print('\033[31m ' + s + ' \033[0m')
+    def printV(self, s):
+        print('\033[33m ' + s + ' \033[0m')
 
-def printI(s):
-    print('\033[32m ' + s + ' \033[0m')
+    def getJsonData(self, url, time=5):
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
 
-def printV(s):
-    print('\033[33m ' + s + ' \033[0m')
+        headers = {'User-Agent': user_agent}
 
-def URLTools(url, time=5):
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        req = urllib.request.Request(url, headers=headers)
 
-    headers = {'User-Agent': user_agent}
-
-    req = urllib.request.Request(url, headers=headers)
-
-    try:
-        response = urllib.request.urlopen(req, timeout=5)
-        json_txt = response.read().decode("utf8")
-        return json_txt
-    except (HTTPError, URLError, socket.timeout, socket.gaierror) as e:
-        print('URLTools exception %s' % e)
-        if time > 0:
-            time -= 1
-            return URLTools(url, time)
-        else:
-            cmdline = 'echo ' + url + ' >> ./errorjson.txt'
-            os.system(cmdline)
-
-def getDownloadPicUrl(string):
-    jsonObj = json.loads(string)
-    pic_list = []
-    if 'data' in jsonObj:
-        data = jsonObj['data']
-        after = None
-        if 'after' in data:
-            after = data['after']
-            #print('after %s ' % after)
-        if 'children' in data:
-            chinldren = data['children']
-            for child in chinldren:
-                data = child['data']
-                pic_url = data['url']
-                #print('is_video %s  pic url %s' % (data['is_video'], pic_url))
-                if data['is_video'] is False:
-                    pic_list.append(pic_url)
-    return after, pic_list
-
-def is_valid_jpg(jpg_file):
-    with open(jpg_file, 'rb') as f:
-        f.seek(-2, 2)
-        buf = f.read()
-        f.close()
-        return buf == b'\xff\xd9'  # 判定jpg是否包含结束字段
-
-def is_valid_png(png_file):
-    with open(png_file, 'rb') as f:
-        f.seek(-2, 2)
-        buf = f.read()
-        return buf == b'\x60\x82'
-
-def DownloadFile(path, url, time=5):
-    md5_val = hashlib.md5(url.encode('utf8')).hexdigest()
-    jpeg_full_name = path + '/' + md5_val + '.jpeg'
-    png_full_name = path + '/' + md5_val + '.png'
-
-    if os.path.exists(jpeg_full_name):
-        printI('file exist %s ' % jpeg_full_name)
-        if is_valid_jpg(jpeg_full_name) is False:
-            os.remove(jpeg_full_name)
-            printI('remove pic %s ' % jpeg_full_name)
-        else:
-            return
-    elif os.path.exists(png_full_name):
-        printI('file exist %s ' % png_full_name)
-        if is_valid_png(png_full_name) is False:
-            os.remove(png_full_name)
-            printI('remove pic %s ' % png_full_name)
-        else:
-            return
-
-    try:
-        s = requests.session()
-        s.keep_alive = False
-        r = s.get(url, headers={'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'})
-        headers = r.headers
-        if 'Content-Type' in headers:
-            dtype = headers['Content-Type']
-            if dtype == 'image/jpeg' or dtype == 'image/png':
-                pic_full_name = path + '/' + md5_val + '.' + dtype.split('/')[1]
-                printI('pic_name : %s, url : %s' % (pic_full_name, url))
-                with open(pic_full_name, 'wb') as f:
-                    f.write(r.content)
+        try:
+            response = urllib.request.urlopen(req, timeout=5)
+            json_txt = response.read().decode("utf8")
+            return json_txt
+        except (HTTPError, URLError, socket.timeout, socket.gaierror) as e:
+            print('getJsonData exception %s' % e)
+            if time > 0:
+                time -= 1
+                return self.getJsonData(url, time)
             else:
-                cmdline = 'echo ' + 'path : ' + path + ', url : ' + url + ', type : ' + dtype + ' >> ' + path + '/typeerror.txt'
+                cmdline = 'echo ' + url + ' >> ./errorjson.txt'
+                os.system(cmdline)
+                return None
+
+    def getDownloadPicUrl(string):
+        jsonObj = json.loads(string)
+        pic_list = []
+        after = None
+        if 'data' in jsonObj:
+            data = jsonObj['data']
+            if 'after' in data:
+                after = data['after']
+            if 'children' in data:
+                chinldren = data['children']
+                for child in chinldren:
+                    data = child['data']
+                    pic_url = data['url']
+                    # print('is_video %s  pic url %s' % (data['is_video'], pic_url))
+                    if data['is_video'] is False:
+                        pic_list.append(pic_url)
+        return after, pic_list
+
+    def isValidJPEG(jpg_file):
+        with open(jpg_file, 'rb') as f:
+            f.seek(-2, 2)
+            buf = f.read()
+            f.close()
+            return buf == b'\xff\xd9'  # 判定jpg是否包含结束字段
+
+    def isValidPNG(png_file):
+        with open(png_file, 'rb') as f:
+            f.seek(-2, 2)
+            buf = f.read()
+            return buf == b'\x60\x82'
+
+    def downloadFile(self, path, url, time=5):
+        md5_val = hashlib.md5(url.encode('utf8')).hexdigest()
+        jpeg_full_name = path + '/' + md5_val + '.jpeg'
+        png_full_name = path + '/' + md5_val + '.png'
+
+        if os.path.exists(jpeg_full_name):
+            self.printI('file exist %s ' % jpeg_full_name)
+            if self.isValidJPEG(jpeg_full_name) is False:
+                os.remove(jpeg_full_name)
+                self.printI('remove pic %s ' % jpeg_full_name)
+            else:
+                return
+        elif os.path.exists(png_full_name):
+            self.printI('file exist %s ' % png_full_name)
+            if self.isValidPNG(png_full_name) is False:
+                os.remove(png_full_name)
+                self.printI('remove pic %s ' % png_full_name)
+            else:
+                return
+
+        try:
+            s = requests.session()
+            s.keep_alive = False
+            r = s.get(url, headers={
+                'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'})
+            headers = r.headers
+            if 'Content-Type' in headers:
+                dtype = headers['Content-Type']
+                if dtype == 'image/jpeg' or dtype == 'image/png':
+                    pic_full_name = path + '/' + md5_val + '.' + dtype.split('/')[1]
+                    self.printI('pic_name : %s, url : %s' % (pic_full_name, url))
+                    with open(pic_full_name, 'wb') as f:
+                        f.write(r.content)
+                else:
+                    cmdline = 'echo ' + 'path : ' + path + ', url : ' + url + ', type : ' + dtype + ' >> ' + path + '/typeerror.txt'
+                    os.system(cmdline)
+
+        except Exception as e:
+            self.printE('downloadFile error : %s ' % e)
+            if time > 0:
+                time -= 1
+                self.printE("downloadFile faild %s " % url)
+                self.downloadFile(path, url, time)
+            else:
+                self.printE('file: %s downlaod alway faild' % url)
+                cmdline = 'echo ' + url + ' >> ' + path + '/errorurl.txt'
                 os.system(cmdline)
 
-    except Exception as e:
-        printE('DownloadFile error : %s ' % e)
-        if time > 0:
-            time -= 1
-            printE("DownloadFile faild %s " % url)
-            DownloadFile(path, url, time)
-        else:
-            printE('file: %s downlaod alway faild' % url)
-            cmdline = 'echo ' + url + ' >> ' + path + '/errorurl.txt'
-            os.system(cmdline)
+    def downloadPics(self, path, url_list):
+        for url in url_list:
+            self.downloadFile(path, url)
 
-def DownloadPics(path, url_list):
-    for url in url_list:
-        DownloadFile(path, url)
+    def downloaderCall(self, m):
+        org_url = m['url']
+        download_path = m['path']
+        url = org_url
 
-def DownloadThread(m):
-    org_url = m['url']
-    download_path = m['path']
-    url = org_url
+        self.printV('%s, download path %s, url : %s ' % (threading.current_thread().getName(), download_path, url))
 
-    printV('%s, download path %s, url : %s ' % (threading.current_thread().getName(), download_path, url))
-
-    while True:
-        jsonString = URLTools(url)
-        if jsonString is None:
-            printE(url + '   no no no no no no no no no no no no no  work ')
-        else:
-            printI(url + '   work work work work work work work work work good ')
-
-            after, pic_list = getDownloadPicUrl(jsonString)
-            #print(downloadPath, pic_list)
-            DownloadPics(download_path, pic_list)
-            if after is None:
-                printE('url %s after is None' % org_url)
-                break;
+        while True:
+            jsonString = self.getJsonData(url)
+            if jsonString is None:
+                self.printE(url + '   no no no no no no no no no no no no no  work ')
+                break
             else:
-                afterUrl = org_url + "&after=" + after
-                #print('after url : ' + afterUrl)
-                url = afterUrl
+                self.printI(url + '   work work work work work work work work work good ')
 
-curPath = os.path.abspath('.')
-parentPath = os.path.abspath('..')
-raw_data = parentPath + '/raw_data/'
-source_urls = curPath + '/source_urls/'
+                after, pic_list = self.getDownloadPicUrl(jsonString)
+                # print(downloadPath, pic_list)
+                self.downloadPics(download_path, pic_list)
+                if after is None:
+                    self.printE('url %s after is None' % org_url)
+                    break
+                else:
+                    afterUrl = org_url + "&after=" + after
+                    # print('after url : ' + afterUrl)
+                    url = afterUrl
+
+    def downloader(self, download_list):
+        print(download_list)
+        with ThreadPoolExecutor(10) as executor1:
+            executor1.map(self.downloaderCall, download_list)
+
 
 def main():
+    curPath = os.path.abspath('.')
+    parentPath = os.path.abspath('..')
+    raw_data = parentPath + '/raw_data/'
+    source_urls = curPath + '/source_urls/'
 
     fileList = os.listdir(source_urls)
     download_list = []
@@ -183,8 +176,8 @@ def main():
             d['url'] = line.strip()
             download_list.append(d)
 
-    with ThreadPoolExecutor(10) as executor1:
-        executor1.map(DownloadThread, download_list)
+    d = GetPicsFromWebPage()
+    d.downloader(download_list)
 
 
 if __name__ == '__main__':
